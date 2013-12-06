527a1774549a8e30de84d1992ce59d2600269097
kenta <kenta@hatano-macbook.local>
2013-12-06 13:19:09 +0900

    Initial Commit

---
+++ HSPMac/HSPDocument.h
@@ -0,0 +1,13 @@
+//
+//  HSPDocument.h
+//  HSPMac
+//
+//  Created by kenta on 2013/12/06.
+//  Copyright (c) 2013年 kenta. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HSPDocument : NSDocument
+
+@end
---
+++ HSPMac/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HSPMac
+//
+//  Created by kenta on 2013/12/06.
+//  Copyright (c) 2013年 kenta. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
